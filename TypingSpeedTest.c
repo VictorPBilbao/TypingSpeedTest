4f5929bbc57ce925e5f67c22b4f00767811fb4b5
VictorPBilbao <victor.pasini.bilbao@gmail.com>
2024-04-07 14:34:59 -0300

    Refactor TypingSpeedTest.c to improve code structure and readability, and add show_statistics function

--- TypingSpeedTest.c
+++ TypingSpeedTest.c
@@ -6,6 +6,9 @@
 #include <windows.h>
 #include <locale.h>
 
+char original_text[] = "The quick brown fox jumps over the lazy dog.";
+char user_text[100] = "";
+
 void print_main_menu()
 {
     int choice;
@@ -92,13 +95,30 @@ void print_speed(int press_count, int err_count, time_t elapsed_time)
     printf("\033[0;33mElapsed time: %.2f seconds\033[0m\n", (double)elapsed_time);
 }
 
+void show_statistics(time_t start_time, time_t end_time, int press_count, int err_count)
+{
+    double total;
+
+    system("cls");
+
+    end_time = time(NULL); // get the end time
+
+    total = difftime(end_time, start_time); // calculate elapsed time in seconds
+
+    double words_per_minute = (strlen(user_text) / 5.0) / (total / 60.0); // calculate words per minute
+
+    printf("Congratulations! You have typed the text correctly!: \033[0;33m%s\033[0m \n", user_text);
+
+    print_speed(press_count, err_count, total);
+    printf("Words per minute: %.2f\n", words_per_minute);
+}
+
 int main()
 {
     system("cls");
 
     // char original_text[] = "A Hare was making fun of the Tortoise one day for being so slow. 'Do you ever get anywhere?' he asked with a mocking laugh. 'Yes,' replied the Tortoise, 'and I get there sooner than you think. I'll run you a race and prove it.' The Hare was much amused at the idea of running a race with the Tortoise, but for the fun of the thing he agreed. So the Fox, who had consented to act as judge, marked the distance and started the runners off. The Hare was soon far out of sight, and to make the Tortoise feel very deeply how ridiculous it was for him to try a race with a Hare, he lay down beside the course to take a nap until the Tortoise should catch up. The Tortoise meanwhile kept going slowly but steadily, and, after a time, passed the place where the Hare was sleeping. But the Hare slept on very peacefully; and when at last he did wake up, the Tortoise was near the goal. The Hare now ran his swiftest, but he could not overtake the Tortoise in time.";
-    char original_text[] = "The quick brown fox jumps over the lazy dog.";
-    char user_text[100] = "";
+
     int keypress;
     char next_letter[2];
     int index_position = 0;
@@ -114,7 +134,6 @@ int main()
     hidecursor();
     print_main_menu();
     get_menu_selection();
-
     print_text(original_text, user_text);
 
     while (1)
@@ -145,18 +164,7 @@ int main()
         print_speed(press_count, err_count, elapsed_time);
     }
 
-    system("cls");
-
-    end_time = time(NULL); // get the end time
-
-    elapsed_time = difftime(end_time, start_time); // calculate elapsed time in seconds
-
-    double words_per_minute = (strlen(user_text) / 5.0) / (elapsed_time / 60.0); // calculate words per minute
-
-    printf("Congratulations! You have typed the text correctly!: \033[0;33m%s\033[0m \n", user_text);
-
-    print_speed(press_count, err_count, elapsed_time);
-    printf("Words per minute: %.2f\n", words_per_minute);
+    show_statistics(start_time, end_time, press_count, err_count);
 
     return 0;
 }
