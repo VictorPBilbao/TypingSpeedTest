1d190c1503dfcfe2bcb88ae015436b049c31da48
VictorPBilbao <victor.pasini.bilbao@gmail.com>
2024-04-07 14:24:23 -0300

    Refactor TypingSpeedTest.c to improve code structure and readability

--- TypingSpeedTest.c
+++ TypingSpeedTest.c
@@ -6,45 +6,6 @@
 #include <windows.h>
 #include <locale.h>
 
-void hidecursor()
-{
-    HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
-    CONSOLE_CURSOR_INFO info;
-    info.dwSize = 100;
-    info.bVisible = FALSE;
-    SetConsoleCursorInfo(consoleHandle, &info);
-}
-
-void set_cursor_position(int x, int y)
-{
-    COORD coord;
-    coord.X = x;
-    coord.Y = y;
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleCursorPosition(hConsole, coord);
-}
-
-void print_text(char *original_text, char *user_text)
-{
-    printf("\033[0;32m%s\033[0m", user_text);
-    printf("\033[0;33m%s\033[0m \n", original_text + strlen(user_text));
-}
-
-void print_speed(int press_count, int err_count, time_t start_time)
-{
-    printf("Total of keypresses: %d\n", press_count);
-    printf("Total of errors: %d\n", err_count);
-    if (press_count > 0)
-    {
-        printf("Accuracy: %.2f%%\n", (1 - (err_count / (float)press_count)) * 100);
-    }
-    else
-    {
-        printf("Accuracy: 100%%\n");
-    }
-    printf("Elapsed time: %.4f seconds\n", difftime(time(NULL), start_time));
-}
-
 void print_main_menu()
 {
     int choice;
@@ -92,11 +53,51 @@ void get_menu_selection()
     }
 }
 
+void hidecursor()
+{
+    HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO info;
+    info.dwSize = 100;
+    info.bVisible = FALSE;
+    SetConsoleCursorInfo(consoleHandle, &info);
+}
+
+void set_cursor_position(int x, int y)
+{
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleCursorPosition(hConsole, coord);
+}
+
+void print_text(char *original_text, char *user_text)
+{
+    printf("\033[0;32m%s\033[0m", user_text);
+    printf("%s \n", original_text + strlen(user_text));
+}
+
+void print_speed(int press_count, int err_count, time_t elapsed_time)
+{
+    printf("Total of keypresses: %d\n", press_count);
+    printf("Total of errors: %d\n", err_count);
+    if (press_count > 0)
+    {
+        printf("Accuracy: %.2f%%\n", (1 - (err_count / (float)press_count)) * 100);
+    }
+    else
+    {
+        printf("Accuracy: 100%%\n");
+    }
+    printf("\033[0;33mElapsed time: %.2f seconds\033[0m\n", (double)elapsed_time);
+}
+
 int main()
 {
     system("cls");
 
-    char original_text[] = "A Hare was making fun of the Tortoise one day for being so slow. 'Do you ever get anywhere?' he asked with a mocking laugh. 'Yes,' replied the Tortoise, 'and I get there sooner than you think. I'll run you a race and prove it.' The Hare was much amused at the idea of running a race with the Tortoise, but for the fun of the thing he agreed. So the Fox, who had consented to act as judge, marked the distance and started the runners off. The Hare was soon far out of sight, and to make the Tortoise feel very deeply how ridiculous it was for him to try a race with a Hare, he lay down beside the course to take a nap until the Tortoise should catch up. The Tortoise meanwhile kept going slowly but steadily, and, after a time, passed the place where the Hare was sleeping. But the Hare slept on very peacefully; and when at last he did wake up, the Tortoise was near the goal. The Hare now ran his swiftest, but he could not overtake the Tortoise in time.";
+    // char original_text[] = "A Hare was making fun of the Tortoise one day for being so slow. 'Do you ever get anywhere?' he asked with a mocking laugh. 'Yes,' replied the Tortoise, 'and I get there sooner than you think. I'll run you a race and prove it.' The Hare was much amused at the idea of running a race with the Tortoise, but for the fun of the thing he agreed. So the Fox, who had consented to act as judge, marked the distance and started the runners off. The Hare was soon far out of sight, and to make the Tortoise feel very deeply how ridiculous it was for him to try a race with a Hare, he lay down beside the course to take a nap until the Tortoise should catch up. The Tortoise meanwhile kept going slowly but steadily, and, after a time, passed the place where the Hare was sleeping. But the Hare slept on very peacefully; and when at last he did wake up, the Tortoise was near the goal. The Hare now ran his swiftest, but he could not overtake the Tortoise in time.";
+    char original_text[] = "The quick brown fox jumps over the lazy dog.";
     char user_text[100] = "";
     int keypress;
     char next_letter[2];
@@ -106,12 +107,11 @@ int main()
 
     int x = 0, y = 0; // Set these to the position where you want to start drawing
 
-    hidecursor();
-
-    time_t start_time, end_time, current_time;
+    time_t start_time, end_time;
     double elapsed_time;
     start_time = time(NULL); // get the start time
 
+    hidecursor();
     print_main_menu();
     get_menu_selection();
 
@@ -119,6 +119,7 @@ int main()
 
     while (1)
     {
+        elapsed_time = difftime(time(NULL), start_time);
         set_cursor_position(x, y);
         if (kbhit())
         {
@@ -141,7 +142,7 @@ int main()
             break;
         }
         print_text(original_text, user_text);
-        print_speed(press_count, err_count, start_time);
+        print_speed(press_count, err_count, elapsed_time);
     }
 
     system("cls");
@@ -154,7 +155,7 @@ int main()
 
     printf("Congratulations! You have typed the text correctly!: \033[0;33m%s\033[0m \n", user_text);
 
-    print_speed(press_count, err_count, start_time);
+    print_speed(press_count, err_count, elapsed_time);
     printf("Words per minute: %.2f\n", words_per_minute);
 
     return 0;
